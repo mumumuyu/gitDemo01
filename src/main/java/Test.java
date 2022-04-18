4cddf3fd4873d251a46cb57ba6b11e6a8cd9c5ae
lgd <1017276522@qq.com>
2022-04-18 20:20:55 +0800

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @Description:
+ * @author: LGD
+ * @date:2022/4/18 20:20
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello~Git~~~");
+    }
+}
